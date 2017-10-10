d57a7665b1dc035b8977fe812752a5bf0d07bf3e
linhth <linhtranhai87@gmail.com>
2017-10-10 14:57:09 +0700

    fgfgfgfgfgfgfgfgfg

--- amecApp003/amecApp003/AppDelegate.swift
+++ amecApp003/amecApp003/AppDelegate.swift
@@ -13,6 +13,9 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // test
+    //////// amec
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
